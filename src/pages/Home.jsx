8467e30db0a55c663a795c411fbeed8aee8cf309
João Victor Soares <135281593+joaovicsoares@users.noreply.github.com>
2024-07-21 20:31:36 -0300

    responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,18 +10,20 @@ function Home() {
         <div className='homeContainer'>
             <Header />
             <MainImg/>
-            <section>
+            <section id='project'>
                 <h1>Projeto</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum turpis, condimentum ut erat quis, eleifend consectetur eros. Nullam nunc sapien, pellentesque maximus feugiat vitae, vehicula id nisi. Nam posuere, turpis ac suscipit imperdiet, lacus felis sodales ligula, et aliquam ante dui et turpis. Donec dui ante, elementum eu felis a, egestas luctus mauris. Sed placerat pharetra justo, quis rutrum risus commodo vel. Proin vehicula augue dolor. Donec fermentum quam vel mi volutpat sagittis. Nunc elementum mi blandit ligula venenatis placerat. In tempor eu turpis vitae ornare. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla at est ultrices, pellentesque leo at, pulvinar est. Morbi pretium pellentesque ipsum, et malesuada velit molestie in. Sed gravida pulvinar imperdiet.</p>
-                <ArticleCard title="Nossa Missão" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum turpis, condimentum ut erat quis, eleifend consectetur eros. Nullam nunc sapien, pellentesque maximus feugiat vitae, vehicula id nisi" articleImg={ArticleImg} altText="imagem projeto"/>
-                <ArticleCard title="Origem" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum turpis, condimentum ut erat quis, eleifend consectetur eros. Nullam nunc sapien, pellentesque maximus feugiat vitae, vehicula id nisi" articleImg={ArticleImg} altText="imagem projeto"/>
-                <ArticleCard title="Equipe" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum turpis, condimentum ut erat quis, eleifend consectetur eros. Nullam nunc sapien, pellentesque maximus feugiat vitae, vehicula id nisi" articleImg={ArticleImg} altText="imagem projeto"/>
+                <section className='cardSection'>
+                    <ArticleCard title="Nossa Missão" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum turpis, condimentum ut erat quis, eleifend consectetur eros. Nullam nunc sapien, pellentesque maximus feugiat vitae, vehicula id nisi" articleImg={ArticleImg} altText="imagem projeto"/>
+                    <ArticleCard title="Origem" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum turpis, condimentum ut erat quis, eleifend consectetur eros. Nullam nunc sapien, pellentesque maximus feugiat vitae, vehicula id nisi" articleImg={ArticleImg} altText="imagem projeto"/>
+                    <ArticleCard title="Equipe" content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum turpis, condimentum ut erat quis, eleifend consectetur eros. Nullam nunc sapien, pellentesque maximus feugiat vitae, vehicula id nisi" articleImg={ArticleImg} altText="imagem projeto"/>
+                </section>
             </section>
-            <section>
+            <section id='posts'>
                 <h1>Ultimas Postagens</h1>
                 <PostsSection />
             </section>
-            <section>
+            <section id='contact'>
                 <h1>Fale Conosco</h1>
                 <ContactSection/>
             </section>
