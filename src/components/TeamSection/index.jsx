6a0bf5b7d6c1a033d0a71d5da0afb4e1a55a7ea9
João Victor Soares <135281593+joaovicsoares@users.noreply.github.com>
2024-09-18 02:24:06 -0300

    Revert "bootstrap$teamsection"
    This reverts commit f5a94430f942834778a203a101ba5ff005ea8c0e.

--- src/components/TeamSection/index.jsx
+++
@@ -1,9 +0,0 @@
-function TeamSection () {
-    return (
-        <section>
-
-        </section>
-    )
-}
-
-export default TeamSection
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './style/globals.scss'
-import * as bootstrap from 'bootstrap'
+import './style/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
